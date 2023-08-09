56e84757f01772a415ac081478db58e06be3d028
dawit <dawitbogalem@gmail.com>
2023-08-09 18:37:39 -0400

    Updates

--- postcss.config.js
+++
@@ -1,5 +0,0 @@
-const tailwindcss = require('tailwindcss');
-//import tailwindcss from 'tailwindcss';
-module.exports = {
-  plugins: ['postcss-preset-env', tailwindcss('./tailwind.config.js')],
-};
